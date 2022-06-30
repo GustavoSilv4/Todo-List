f7956f52db525fb11ed590fb980f73e41ecd1e9f
GustavoSilv4 <GustavoSilv4.contato@gmail.com>
2022-06-30 16:56:54 -0300

    feat: finished functionality of creating a new item in the list

--- src/components/TaskBar.tsx
+++ src/components/TaskBar.tsx
@@ -1,15 +1,57 @@
+import { ChangeEvent, FormEvent, useState } from 'react';
 import { ClipboardText, PlusCircle } from 'phosphor-react';
+
+import { v4 as uuidv4 } from 'uuid';
+
 import styles from './TaskBar.module.css';
 import { Tasks } from './Tasks';
 
+interface TaskList {
+  id: string;
+  task: string;
+  concluded: boolean;
+}
+
 export function TaskBar() {
+  const [newTask, setNewTask] = useState('');
+  const [tasks, setTasks] = useState<TaskList[]>([
+    {
+      id: '1',
+      task: 'Integer vel sed fames integer auctor neque turpis turpis semper. Duis vel sed fames integer. natus inventore a ',
+      concluded: false,
+    },
+    {
+      id: '2',
+      task: 'Acordar e fazer o trabalho de casa.',
+      concluded: false,
+    },
+  ]);
+
+  function handleNewTaskChange(event: ChangeEvent<HTMLInputElement>) {
+    setNewTask(event.target.value);
+  }
+
+  function handleCreateNewTask(event: FormEvent) {
+    event.preventDefault();
+
+    const newTaskObj = {
+      id: uuidv4(),
+      task: newTask,
+      concluded: false,
+    };
+
+    setTasks([...tasks, newTaskObj]);
+  }
+
   return (
     <div className={styles.TaskBarContainer}>
-      <form className={styles.newTaskForm}>
+      <form onSubmit={handleCreateNewTask} className={styles.newTaskForm}>
         <input
           className={styles.newTaskInput}
           type='text'
           placeholder='Adicione uma nova tarefa'
+          value={newTask}
+          onChange={handleNewTaskChange}
         />
 
         <button className={styles.newTaskButton} type='submit'>
@@ -20,7 +62,7 @@ export function TaskBar() {
 
       <header className={styles.TaskBarHeader}>
         <div className={styles.TaskBarCreated}>
-          Tarefas criadas <span>0</span>
+          Tarefas criadas <span>{tasks.length}</span>
         </div>
         <div className={styles.TaskBarCompleted}>
           Concluídas <span>0</span>
@@ -31,8 +73,14 @@ export function TaskBar() {
         {/* <ClipboardText size={56} weight='thin' />
         <strong>Voce ainda não tem tarefas cadastradas</strong>
         <p>Crie tarefas e organize seus itens a fazer</p> */}
-        <Tasks />
-        <Tasks />
+        {tasks.map((task) => (
+          <Tasks
+            key={task.id}
+            task={task.task}
+            concluded={task.concluded}
+            id={''}
+          />
+        ))}
       </div>
     </div>
   );
--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -2,19 +2,22 @@ import styles from './Tasks.module.css';
 
 import { Trash } from 'phosphor-react';
 
-export function Tasks() {
+interface TasksProps {
+  id: string;
+  task: string;
+  concluded: boolean;
+}
+
+export function Tasks({ id, task, concluded }: TasksProps) {
   return (
     <div className={styles.TasksContainer}>
       <form>
         <input className={styles.TasksInput} type='checkbox' />
-        <span>
-          Integer urna interdum massa libero auctor neque turpis turpis semper.
-          Duis vel sed fames integer.
-        </span>
-        <button>
-          <Trash size={25} />
-        </button>
+        <span>{task}</span>
       </form>
+      <button>
+        <Trash size={20} />
+      </button>
     </div>
   );
 }
